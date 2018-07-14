5e7e8db2ac4b6f280d70ecb24dce502eadec4477
alex-taxiera <alex.taxiera@gmail.com>
2018-07-14 18:30:46 -0400

    disable import routes

--- api/index.js
+++ api/index.js
@@ -4,8 +4,8 @@ const heroes = require('./heroes.js')
 
 api.get('/', (req, res) => run('echo', {message: 'heck'}).then((content) => res.send(content)))
 
-api.get('/importHeroes', (req, res) => run('importHeroes').then((content) => res.send(content)))
-api.get('/importCards', (req, res) => run('importCards').then((content) => res.send(content)))
+// api.get('/importHeroes', (req, res) => run('importHeroes').then((content) => res.send(content)))
+// api.get('/importCards', (req, res) => run('importCards').then((content) => res.send(content)))
 
 api.use('/heroes', heroes)
 

6b8111c30e27f22c1535783bc15a34addfb7a27c
alex-taxiera <alex.taxiera@gmail.com>
2018-07-14 16:32:58 -0400

    add api routes for importing

--- api/index.js
+++ api/index.js
@@ -3,4 +3,7 @@ const { run } = require('../cloud')
 
 api.get('/', (req, res) => run('echo', {message: 'heck'}).then((content) => res.send(content)))
 
+api.get('/importHeroes', (req, res) => run('importHeroes').then((content) => res.send(content)))
+api.get('/importCards', (req, res) => run('importCards').then((content) => res.send(content)))
+
 module.exports = api

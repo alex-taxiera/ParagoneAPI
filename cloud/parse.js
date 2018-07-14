63636694352027965128bfd5eb1f887ec570f08c
alex-taxiera <alex.taxiera@gmail.com>
2018-07-14 18:10:34 -0400

    add some production vs dev tokens

--- cloud/parse.js
+++ cloud/parse.js
@@ -1,6 +1,6 @@
 const Parse = require('parse/node')
 
 Parse.initialize(process.env.PARSE_APP_ID, process.env.PARSE_MASTER_KEY)
-Parse.serverURL = process.env.PARSE_SERVER_URL
+Parse.serverURL = process.env.inProduction === true ? process.env.PARSE_SERVER_URL : process.env.PARSE_LOCAL_URL
 
 module.exports = Parse
--- index.js
+++ index.js
@@ -2,12 +2,13 @@ require('dotenv').load()
 const path = require('path')
 const app = require('express')()
 const ParseServer = require('parse-server').ParseServer
+process.env.inProduction = process.env.NODE_ENV === 'production'
 
 const server = new ParseServer({
   databaseURI: process.env.PARSE_DATABASE_URI,
   appId: process.env.PARSE_APP_ID,
   masterKey: process.env.PARSE_MASTER_KEY,
-  serverURL: process.env.PARSE_SERVER_URL,
+  serverURL: process.env.inProduction === true ? process.env.PARSE_SERVER_URL : process.env.PARSE_LOCAL_URL,
   cloud: path.resolve(__dirname, './cloud/main.js')
 })
 

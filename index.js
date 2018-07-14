8c53a2f7c95ab2521acb834bed9b3c20bdffa5af
alex-taxiera <alex.taxiera@gmail.com>
2018-07-14 18:10:47 -0400

    mount parse dashboard with express

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').load()
 const path = require('path')
 const app = require('express')()
 const ParseServer = require('parse-server').ParseServer
+const ParseDashboard = require('parse-dashboard')
 process.env.inProduction = process.env.NODE_ENV === 'production'
 
 const server = new ParseServer({
@@ -12,8 +13,35 @@ const server = new ParseServer({
   cloud: path.resolve(__dirname, './cloud/main.js')
 })
 
+const dashboard = new ParseDashboard({
+  apps: [
+    {
+      appName: 'ParagoneAPI-local',
+      appId: process.env.PARSE_APP_ID,
+      serverURL: process.env.PARSE_LOCAL_URL,
+      masterKey: process.env.PARSE_MASTER_KEY,
+      primaryBackgroundColor: '#575757'
+    },
+    {
+      appName: 'ParagoneAPI-host',
+      appId: process.env.PARSE_APP_ID,
+      serverURL: process.env.PARSE_SERVER_URL,
+      masterKey: process.env.PARSE_MASTER_KEY,
+      production: true,
+      primaryBackgroundColor: '#177587'
+    }
+  ],
+  users: [
+    {
+      user: process.env.PARSE_USER,
+      pass: process.env.PARSE_PASS
+    }
+  ]
+})
+
 const api = require('./api')
 
 app.use('/parse', server)
+app.use('/dashboard', dashboard)
 app.use('/data', api)
 app.listen(3000, () => console.log('online'))

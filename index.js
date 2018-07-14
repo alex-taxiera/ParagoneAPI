e8189e33f4bffcb5bb6c5bd007d984e7116631c5
alex-taxiera <alex.taxiera@gmail.com>
2018-07-13 21:49:34 -0400

    put server initialization in index

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 require('dotenv').load()
+const path = require('path')
 const app = require('express')()
+const ParseServer = require('parse-server').ParseServer
 
-const { server } = require('./cloud')
+const server = new ParseServer({
+  databaseURI: process.env.PARSE_DATABASE_URI,
+  appId: process.env.PARSE_APP_ID,
+  masterKey: process.env.PARSE_MASTER_KEY,
+  serverURL: process.env.PARSE_SERVER_URL,
+  cloud: path.resolve(__dirname, './cloud/main.js')
+})
+
 const api = require('./api')
 
 app.use('/parse', server)

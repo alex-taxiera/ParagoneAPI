a2c1853c301307f381967d650cbdb96950357aca
alex-taxiera <alex.taxiera@gmail.com>
2018-07-14 17:19:13 -0400

    define endpoint for all hero related data

--- cloud/main.js
+++ cloud/main.js
@@ -12,6 +12,21 @@ defineEndpoint('echo',
   }
 )
 
+defineEndpoint('getHeroesAll',
+  async (req, res) => {
+    const heroesData = await new Parse.Query(Parse.Object.extend('Hero')).find()
+    const heroes = heroesData.map(async (hero) => {
+      const json = hero.toJSON()
+      const abilities = await hero.get('abilities').query().find()
+      json.abilities = abilities.map((ability) => ability.toJSON())
+      const attributes = await hero.get('attributes').query().find()
+      json.attributes = attributes.map((attribute) => attribute.toJSON()).sort((a, b) => a.level - b.level)
+      return json
+    })
+    Promise.all(heroes).then((response) => res.success(response))
+  }
+)
+
 defineEndpoint('importCards',
   (req, res) => {
     const Card = Parse.Object.extend('Card')

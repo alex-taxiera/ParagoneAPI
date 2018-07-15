0dfbc28f40ea71ad7ea564ce64fe07888f0ee91a
alex-taxiera <alex.taxiera@gmail.com>
2018-07-15 16:25:40 -0400

    add route for card info

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 const api = require('express').Router()
 const { run } = require('../cloud')
 const heroes = require('./heroes.js')
+const cards = require('./cards.js')
 
 api.get('/', (req, res) => run('echo', {message: 'heck'}).then((content) => res.send(content)))
 
-api.get('/importHeroes', (req, res) => run('importHeroes').then((content) => res.send(content)))
-api.get('/importCards', (req, res) => run('importCards').then((content) => res.send(content)))
+// api.get('/importHeroes', (req, res) => run('importHeroes').then((content) => res.send(content)))
+// api.get('/importCards', (req, res) => run('importCards').then((content) => res.send(content)))
 
 api.use('/heroes', heroes)
+api.use('/cards', cards)
 
 module.exports = api

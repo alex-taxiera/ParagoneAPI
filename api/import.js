84b94008cdf110f43ee3a9819c90432120958466
alex-taxiera <alex.taxiera@gmail.com>
2018-07-15 21:00:23 -0400

    modularize import routes

---
+++ api/import.js
@@ -0,0 +1,19 @@
+const importers = require('express').Router()
+const { run } = require('../cloud')
+
+const runCloud = async (req, res) => {
+  const runner = async (req, res) => {
+    switch (req.params.type) {
+      case 'cards': return run('importCards')
+      case 'heroes': return run('importHeroes')
+      default: return run('importHeroes').then(() => run('importCards'))
+    }
+  }
+  return runner(req, res)
+    .then((content) => res.send(content))
+    .catch((err) => res.status(err.code).send(err.message))
+}
+
+importers.get('/:type', runCloud)
+
+module.exports = importers
--- api/index.js
+++ api/index.js
@@ -2,12 +2,11 @@ const api = require('express').Router()
 const { run } = require('../cloud')
 const heroes = require('./heroes.js')
 const cards = require('./cards.js')
+const imports = require('./import.js')
 
 api.get('/', (req, res) => run('echo', {message: 'main'}).then((content) => res.send(content)))
 
-// api.get('/importHeroes', (req, res) => run('importHeroes').then((content) => res.send(content)))
-// api.get('/importCards', (req, res) => run('importCards').then((content) => res.send(content)))
-
+// api.use('/import', imports)
 api.use('/heroes', heroes)
 api.use('/cards', cards)
 

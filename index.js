12b77e6fa0477ea2640e52ed69cade36b73da6b0
alex-taxiera <alex.taxiera@gmail.com>
2018-10-11 13:00:47 -0400

    enforce https

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const enforce = require('express-sslify')
 
 const IN_PRODUCTION = process.env.NODE_ENV === 'production'
 if (!IN_PRODUCTION) require('dotenv').load()
@@ -15,6 +16,7 @@ const URL = IN_PRODUCTION ? SERVER_URL : LOCAL_URL + ':' + PORT
 const app = express()
 const paragone = require('./src/api/v1')
 
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 app.set('json spaces', 2)
 
 app.use(API_ROUTE, paragone)
